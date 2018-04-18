b2aa27769ad6c7a839613a1e02fe19676d550902
Lorena <lorena.giddens@outlook.com>
2018-04-18 18:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-    rock.style.top = `${2}px`;
+    rock.style.top = `${top++2}px`;
     if 
 
     /**
