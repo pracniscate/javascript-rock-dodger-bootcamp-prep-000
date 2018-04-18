2b9b9a97dbde536e0e4b4b5b2aae477a2e9142b3
Lorena <lorena.giddens@outlook.com>
2018-04-18 18:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function checkCollision(rock) {
     if ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
     (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) ||
     (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)) {
+      
       return true;
     }
   }
