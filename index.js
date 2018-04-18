a916d4c3ef14912595dba7d38075adf04c650ed2
Lorena <lorena.giddens@outlook.com>
2018-04-18 19:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
   
-  if(e.which === 39) {
+  if(e.which === LEFT_ARROW) {
     moveDodgerLeft();
   } else if(e.which === RIGHT_ARROW) {
       moveDodgerRight();
